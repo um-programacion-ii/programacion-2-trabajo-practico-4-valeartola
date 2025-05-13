d7333356b6e78b1a77baa10b75caf904ad7500a8
valeartola <mv.artola@alumno.um.edu.ar>
2025-05-13 12:27:13 -0300

    implementacion controllers

---
+++ GestionBiblioteca/src/main/java/com/example/demo/controller/UsuarioController.java
@@ -0,0 +1,41 @@
+package com.example.demo.controller;
+
+import com.example.demo.recursos.Usuario;
+import com.example.demo.service.UsuarioService;
+import org.springframework.web.bind.annotation.*;
+import java.util.*;
+
+@RestController
+@RequestMapping("/api/usuarios")
+public class UsuarioController {
+    private final UsuarioService usuarioService;
+
+    public UsuarioController(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+
+    @GetMapping
+    public List<Usuario> obtenerTodos() {
+        return usuarioService.obtenerTodos();
+    }
+
+    @GetMapping("/{id}")
+    public Usuario obtenerPorId(@PathVariable Long id) {
+        return usuarioService.buscarPorId(id);
+    }
+
+    @PostMapping
+    public Usuario crear(@RequestBody Usuario usuario) {
+        return usuarioService.guardar(usuario);
+    }
+
+    @PutMapping("/{id}")
+    public Usuario actualizar(@PathVariable Long id, @RequestBody Usuario usuario) {
+        return usuarioService.actualizar(id, usuario);
+    }
+
+    @DeleteMapping("/{id}")
+    public void eliminar(@PathVariable Long id) {
+        usuarioService.eliminar(id);
+    }
+}
